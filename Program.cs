2b0e58196b176d6a3abd442c2b06afb5d1f47f68
Mustafa.Calkan <Mustafa.Calkan@302-08>
2023-04-01 11:02:09 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace milyoner_githup
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            namespace MilyonerV1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Kim Milyoner Olmak İster - V1
+            Console.WriteLine("Kim Milyoner Olmak İster\n");
+            Console.Write("İsim Giriniz: ");
+            #endregion
+        }
+    }
+}
+        }
+    }
+}
