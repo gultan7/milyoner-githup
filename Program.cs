119f299c221aab54ed7f1dc194a3b74cb973cf2d
gultan7 <gul_tan@hotmail.com>
2023-04-01 11:11:22 +0300

    fead: add joker 1 2

--- Program.cs
+++ Program.cs
@@ -7,9 +7,20 @@
             #region Kim Milyoner Olmak İster - V1
             Console.WriteLine("Kim Milyoner Olmak İster\n");
             Console.Write("İsim Giriniz: ");
+            
+            string isim = Console.ReadLine();
+            Console.Write("Soyisim Giriniz: ");
+            string soyisim = Console.ReadLine();
+
+            Console.Write("\n1.Telefon Jokerinizi Giriniz: ");
+            string telefonJoker1 = Console.ReadLine();
+            Console.Write("2.Telefon Jokerinizi Giriniz: ");
+            string telefonJoker2 = Console.ReadLine();
+            Console.Write("3.Telefon Jokerinizi Giriniz: ");
+            string telefonJoker3 = Console.ReadLine();
 
         }
-        }
+    }
     }
         }
     }
