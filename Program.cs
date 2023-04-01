5d4d46f8781951fe5db3f6ed8a783334c21f6d04
gultan7 <gul_tan@hotmail.com>
2023-04-01 11:07:30 +0300

    feat add title1

--- Program.cs
+++ Program.cs
@@ -4,10 +4,13 @@
     {
         static void Main(string[] args)
         {
-            #region Kim Milyoner Olmak İster\n);
+            #region Kim Milyoner Olmak İster - V1
+            Console.WriteLine("Kim Milyoner Olmak İster\n");
+            Console.Write("İsim Giriniz: ");
+
+        }
         }
     }
-}
         }
     }
 }
